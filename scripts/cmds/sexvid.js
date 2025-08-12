9f61eeaa7b7c2f6cd39d52f61fab154089556d2b
rmd851350-code <rmd851350@gmail.com>
2025-08-12 18:37:36 +0600

    Update sexvid.js

--- scripts/cmds/sexvid.js
+++ scripts/cmds/sexvid.js
@@ -8,7 +8,7 @@ module.exports = {
     version: "2.0",
     author: "nexo_here",
     countDown: 30,
-    role: 2,
+    role: 0,
     shortDescription: "",
     longDescription: "get kanda/p***n video hilake sojaa",
     category: "18+",
